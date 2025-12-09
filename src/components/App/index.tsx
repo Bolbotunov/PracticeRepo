07807e080ee25798435bd713a49857e2907db269
Bolbotunov Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2025-12-09 10:31:03 +0300

    init: default files for app

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,19 +1,11 @@
 import { Provider } from "react-redux";
-import { store } from "../../store/store";
-import Counter from "../Counter";
-import Users from "../Users";
 
 import styles from "./styles.module.scss";
 
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <div className={styles.wrapper}>
-          <Users />
-          <Counter />
-        </div>
-      </Provider>
+      <div className={styles.wrapper}></div>
     </>
   );
 }
